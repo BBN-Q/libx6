660c31c99c2c3b804fcb2fb91897d2f713a3ce9e
Colm Ryan <cryan@bbn.com>
2015-09-30 22:19:34 -0400

    :construction: add firmware version checking to unit test

--- test/test_libx6.cpp
+++ test/test_libx6.cpp
@@ -22,4 +22,32 @@ TEST_CASE("board temperature is sane", "[get_logic_temperature]"){
 		REQUIRE( logicTemp > 20 );
 		REQUIRE( logicTemp < 70 );
 	}
+
+	disconnect_x6(0);
+}
+
+TEST_CASE("firmware version", "[get_firmware_version]") {
+
+	connect_x6(0);
+
+	SECTION("firmware version checks") {
+		uint16_t ver;
+
+		SECTION("BBN firmware is greater than 0.8"){
+			get_firmware_version(0, BBN_X6, &ver);
+			REQUIRE( ver >= 0x0008);
+		}
+
+		SECTION("BBN QDSP firmware is greater than 1.0"){
+			get_firmware_version(0, BBN_QDSP, &ver);
+			REQUIRE( ver >= 0x0100);
+		}
+
+		SECTION("BBN PG firmware is greater than 0.1"){
+			get_firmware_version(0, BBN_PG, &ver);
+			REQUIRE( ver >= 0x0001);
+		}
+	}
+
+	disconnect_x6(0);
 }
