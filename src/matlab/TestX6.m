8c3900b876f99d24d3fbd29d9d5283142be21eec
Colm Ryan <cryan@bbn.com>
2016-02-29 11:40:14 -0500

    :white_check_mark: unit-test coverage for only integrated streams
    covers issue #23

--- src/matlab/TestX6.m
+++ src/matlab/TestX6.m
@@ -295,6 +295,36 @@ classdef TestX6 < matlab.unittest.TestCase
             assertEqual(testCase, KIs, expected, 'AbsTol', 1/2^8);
         end
 
+        function test_only_raw_integrator(testCase)
+            %Test only the integrated raw stream
+            disconnect(testCase.x6);
+            connect(testCase.x6, 0);
+
+            %Enable only one kernel integrator stream
+            enable_stream(testCase.x6, 1, 0, 1);
+
+            %Write a random kernel
+            kernel = (-1.0 + 2*rand(1280,1)) + 1i*(-1.0 + 2*rand(1280,1));
+            write_kernel(testCase.x6, 1, 0, 1, kernel);
+
+            set_averager_settings(testCase.x6, 5120, 64, 1, 1);
+
+            acquire(testCase.x6);
+
+            %enable test mode
+            write_register(testCase.x6, testCase.x6.DSP_WB_OFFSET(1), 1, 65536 + 25000);
+
+            success = wait_for_acquisition(testCase.x6, 1);
+            stop(testCase.x6);
+            assertEqual(testCase, success, 0);
+
+            rawWFs = TestX6.expected_raw_wfs();
+            KIs = transfer_stream(testCase.x6, struct('a', 1, 'b', 0, 'c', 1));
+            expected = sum(bsxfun(@times, kernel, rawWFs(1:length(kernel),:)), 1);
+            assertEqual(testCase, KIs, expected, 'AbsTol', 1/2^4);
+
+        end
+
         function test_demod_kernel_memory(testCase)
 
             baseKernel = (-1.0 + (2-1/2^15)*rand(513,1)) + 1i*(-1.0 + (2-1/2^15)*rand(513,1));
