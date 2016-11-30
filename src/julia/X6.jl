286de08e15f69759b94a553430986b0137037b2b
Blake R. Johnson <bjohnson@bbn.com>
2016-11-30 11:38:49 -0500

    More portable way to load libx6 in X6.jl

--- src/julia/X6.jl
+++ src/julia/X6.jl
@@ -5,8 +5,9 @@ type X6
 end
 X6() = X6(-1)
 
+__init__() = push!(Libdl.DL_LOAD_PATH, joinpath(dirname(@__FILE__), "../../build/"))
+
 const DSP_WB_OFFSET = [0x2000, 0x2100]
-const X6_LIBRARY = "C:\\Users\\qlab\\Documents\\GitHub\\libx6\\build\\libx6"
 const DECIMATION_FACTOR = 4
 
 immutable Channel
@@ -16,7 +17,7 @@ immutable Channel
 end
 
 function connect!(dev::X6, id)
-	ret = ccall((:connect_x6, X6_LIBRARY), Int32, (Int32,), id)
+	ret = ccall((:connect_x6, "libx6"), Int32, (Int32,), id)
 	if ret == 0
 		dev.id = id
 	else
@@ -25,90 +26,90 @@ function connect!(dev::X6, id)
 end
 
 function disconnect!(dev::X6)
-	ret = ccall((:disconnect_x6, X6_LIBRARY), Int32, (Int32,), dev.id)
+	ret = ccall((:disconnect_x6, "libx6"), Int32, (Int32,), dev.id)
 	dev.id = -1
 end
 
 function is_open(dev::X6)
-	bool(ccall((:is_open, X6_LIBRARY), Int32, (Int32,), dev.id))
+	bool(ccall((:is_open, "libx6"), Int32, (Int32,), dev.id))
 end
 
 function set_logging_level(level)
 	#Sets logging level library wide
-	ccall((:set_logging_level, X6_LIBRARY), Int32, (Int32,), level)
+	ccall((:set_logging_level, "libx6"), Int32, (Int32,), level)
 end
 
 function get_logic_temperature(dev::X6)
-	ccall((:get_logic_temperature, X6_LIBRARY), Float32, (Int32, Int32), dev.id, 0)
+	ccall((:get_logic_temperature, "libx6"), Float32, (Int32, Int32), dev.id, 0)
 end
 
 function get_sampleRate(dev::X6)
-	ccall((:get_sampleRate, X6_LIBRARY), Float64, (Int32,), dev.id)
+	ccall((:get_sampleRate, "libx6"), Float64, (Int32,), dev.id)
 end
 
 function set_sampleRate(dev::X6, rate::Real)
-	ccall((:set_sampleRate, X6_LIBRARY), Int32, (Int32, Float64), dev.id, rate)
+	ccall((:set_sampleRate, "libx6"), Int32, (Int32, Float64), dev.id, rate)
 end
 
 function get_trigger_source(dev::X6)
-	ccall((:get_trigger_source, X6_LIBRARY), Int32, (Int32,), dev.id)
+	ccall((:get_trigger_source, "libx6"), Int32, (Int32,), dev.id)
 end
 
 function set_trigger_source(dev::X6, source::Int)
-	ccall((:set_trigger_source, X6_LIBRARY), Int32, (Int32, Int32), dev.id, source)
+	ccall((:set_trigger_source, "libx6"), Int32, (Int32, Int32), dev.id, source)
 end
 
 function get_reference(dev::X6)
 	strMap = {0 => "internal", 1=>"external"}
-	ref = ccall((:get_reference, X6_LIBRARY), Int32, (Int32,), dev.id)
+	ref = ccall((:get_reference, "libx6"), Int32, (Int32,), dev.id)
 	strMap[ref]
 end
 
 function set_reference(dev::X6, ref::String)
 	strMap = {"internal" => 0, "external"=> 1}
-	ccall((:set_reference, X6_LIBRARY), Int32, (Int32, Int32), dev.id, strMap[ref])
+	ccall((:set_reference, "libx6"), Int32, (Int32, Int32), dev.id, strMap[ref])
 end
 
 function enable_stream(dev::X6, a, b, c)
-	ccall((:enable_stream, X6_LIBRARY), Int32, (Int32, Int32, Int32, Int32), dev.id, a, b, c)
+	ccall((:enable_stream, "libx6"), Int32, (Int32, Int32, Int32, Int32), dev.id, a, b, c)
 end
 
 function disable_stream(dev::X6, a, b, c)
-	ccall((:disable_stream, X6_LIBRARY), Int32, (Int32, Int32, Int32, Int32), dev.id, a, b, c)
+	ccall((:disable_stream, "libx6"), Int32, (Int32, Int32, Int32, Int32), dev.id, a, b, c)
 end
 
 function read_register(dev::X6, addr, offset)
 	#Read an address in the wishbone register space
-	ccall((:read_register, X6_LIBRARY), Int32, (Int32, Int32, Int32), dev.id, addr, offset)
+	ccall((:read_register, "libx6"), Int32, (Int32, Int32, Int32), dev.id, addr, offset)
 end
 
 function write_register(dev::X6, addr, offset, value)
 	#Write an address in the wishbone register space
-	ccall((:write_register, X6_LIBRARY), Int32, (Int32, Int32, Int32, Int32), dev.id, addr, offset, value)
+	ccall((:write_register, "libx6"), Int32, (Int32, Int32, Int32, Int32), dev.id, addr, offset, value)
 end
 
 function set_averager_settings(dev::X6, recordLength::Int, numSegments::Int, waveforms::Int, roundRobins::Int)
-	ccall((:set_averager_settings, X6_LIBRARY), Int32, (Int32, Int32, Int32, Int32, Int32),
+	ccall((:set_averager_settings, "libx6"), Int32, (Int32, Int32, Int32, Int32, Int32),
 											dev.id, recordLength, numSegments, waveforms, roundRobins)
 end
 
 function acquire(dev::X6)
-	ccall((:acquire, X6_LIBRARY), Int32, (Int32,), dev.id)
+	ccall((:acquire, "libx6"), Int32, (Int32,), dev.id)
 end
 
 function stop(dev::X6)
-	ccall((:stop, X6_LIBRARY), Int32, (Int32,), dev.id)
+	ccall((:stop, "libx6"), Int32, (Int32,), dev.id)
 end
 
 function wait_for_acquisition(dev::X6, timeOut::Int)
-	ccall((:wait_for_acquisition, X6_LIBRARY), Int32, (Int32, Int32), dev.id, timeOut)
+	ccall((:wait_for_acquisition, "libx6"), Int32, (Int32, Int32), dev.id, timeOut)
 end
 
 function transfer_stream(dev::X6, a, b, c)
 	channels = [Channel(a,b,c)]
-	bufSize = ccall((:get_buffer_size, X6_LIBRARY), Int32, (Int32, Ptr{Channel}, Uint32), dev.id, channels, length(channels))
+	bufSize = ccall((:get_buffer_size, "libx6"), Int32, (Int32, Ptr{Channel}, Uint32), dev.id, channels, length(channels))
 	wfs = Array(Float64, bufSize)
-	success = ccall((:transfer_stream, X6_LIBRARY), Int32, (Int32, Ptr{Channel}, Uint32, Ptr{Float64}, Int32),
+	success = ccall((:transfer_stream, "libx6"), Int32, (Int32, Ptr{Channel}, Uint32, Ptr{Float64}, Int32),
 													dev.id, channels, length(channels), wfs, bufSize)
 	@assert success == 0 "Transferring waveforms failed!"
 	if (b == 0) # physical channel
