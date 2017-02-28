aa8d6665e07f78762b8260cff22d238533cd2ec7
Colm Ryan <colm@colmryan.org>
2017-02-28 15:11:30 -0500

    fancier library git versioning

--- src/lib/version_template.hpp
+++ src/lib/version_template.hpp
@@ -4,7 +4,7 @@
 #include <string>
 using std::string;
 
-const string LIBX6_VERSION = "<TOKEN>";
+const string LIBX6_VERSION = "@GIT_DESCRIBE@";
 string get_driver_version() {
 	return LIBX6_VERSION;
 }
