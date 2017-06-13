10d5519dd1f3094aed0d6b64f7e1d3c20cd8486e
Blake R. Johnson <blake.johnson@raytheon.com>
2017-06-13 13:53:32 -0400

    Version 0.10

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libx6',
-    version="0.9.1",
+    version="0.10",
     url='https://github.com/BBN-Q/libx6',
     py_modules=["libx6"]
 )
