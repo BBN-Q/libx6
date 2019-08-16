3b889eae38e81bfca5b30f4f0fbb337ba2b698b4
gribeill <guilhem.ribeill@gmail.com>
2019-08-16 14:18:00 -0400

    Add register definitions as separate structs.

---
+++ src/lib/X6_registers.cpp
@@ -0,0 +1,52 @@
+// X6_registers.h
+//
+// Register definitions for different firmware versions
+//
+// Original authors: Guilhem Ribeill
+//
+// Copyright 2013-2019 Raytheon BBN Technologies
+
+#include "X6_registers.h"
+
+QDSP_registers::QDSP_registers(const firmware_v10_tag &tag) {
+
+	WB_QDSP_DEMOD_KERNEL_LENGTH			= 0x14;
+	
+	WB_QDSP_RAW_KERNEL_ADDR_DATA		= 0x20;
+	WB_QDSP_DEMOD_KERNEL_ADDR_DATA		= 0x28;
+
+	WB_QDSP_PHASE_INC					= 0x34;
+
+	WB_QDSP_THRESHOLD 					= 0x30;
+	WB_QDSP_THRESHOLD_INVERT			= 0x38;
+	WB_QDSP_THRESHOLD_INPUT_SEL			= 0x00;
+
+	WB_QDSP_RAW_KERNEL_BIAS  			= 0x40;
+	WB_QDSP_DEMOD_KERNEL_BIAS			= 0x48;
+
+	WB_QDSP_CORRELATOR_SIZE				= 0x00;
+	WB_QDSP_CORRELATOR_M_ADDR			= 0x00;
+	WB_QDSP_CORRELATOR_M_DATA			= 0x00;
+	WB_QDSP_CORRELATOR_SEL 				= 0x00;
+
+}
+
+QDSP_registers::QDSP_registers(const int numRawKi, const int numDemod, const firmware_v20_tag &tag) {
+
+	WB_QDSP_DEMOD_KERNEL_LENGTH     = (WB_QDSP_RAW_KERNEL_LENGTH + numRawKi);
+	WB_QDSP_RAW_KERNEL_ADDR_DATA    = (WB_QDSP_DEMOD_KERNEL_LENGTH + numDemod);
+	WB_QDSP_DEMOD_KERNEL_ADDR_DATA  = (WB_QDSP_RAW_KERNEL_ADDR_DATA + 2*numRawKi);
+	WB_QDSP_THRESHOLD               = (WB_QDSP_DEMOD_KERNEL_ADDR_DATA + 2*numDemod);
+	WB_QDSP_PHASE_INC               = (WB_QDSP_THRESHOLD + numRawKi);
+	WB_QDSP_THRESHOLD_INVERT        = (WB_QDSP_PHASE_INC + numRawKi + numDemod);
+	WB_QDSP_THRESHOLD_INPUT_SEL     = (WB_QDSP_THRESHOLD_INVERT + 1);
+	WB_QDSP_RAW_KERNEL_BIAS         = (WB_QDSP_THRESHOLD_INPUT_SEL + 1);
+	WB_QDSP_DEMOD_KERNEL_BIAS       = (WB_QDSP_RAW_KERNEL_BIAS + 2*numRawKi);
+	WB_QDSP_CORRELATOR_SIZE         = (WB_QDSP_DEMOD_KERNEL_BIAS + 2*numDemod);
+	WB_QDSP_CORRELATOR_M_ADDR       = (WB_QDSP_CORRELATOR_SIZE + 1);
+	WB_QDSP_CORRELATOR_M_DATA       = (WB_QDSP_CORRELATOR_M_ADDR + 1);
+	WB_QDSP_CORRELATOR_SEL          = (WB_QDSP_CORRELATOR_M_DATA + 1);
+
+
+
+}
---
+++ src/lib/X6_registers.h
@@ -0,0 +1,56 @@
+// X6_registers.h
+//
+// Register definitions for different firmware versions
+//
+// Original authors: Guilhem Ribeill
+//
+// Copyright 2013-2019 Raytheon BBN Technologies
+
+#ifndef X6_REGISTERS_H_
+#define X6_REGISTERS_H_ 
+
+#include <stdint>
+#include "constants.h"
+
+#define WB_QDSP_TEST                            0x01
+#define WB_QDSP_RECORD_LENGTH                   0x02
+#define WB_QDSP_STREAM_ENABLE                   0x03
+#define WB_QDSP_MODULE_FIRMWARE_VERSION         0x04
+#define WB_QDSP_MODULE_FIRMWARE_GIT_SHA1        0x05
+#define WB_QDSP_MODULE_FIRMWARE_BUILD_TIMESTAMP	0x06
+#define WB_QDSP_NUM_RAW_KI                      0x07
+#define WB_QDSP_NUM_DEMOD                       0x08
+#define WB_QDSP_STATE_VLD_MASK                  0x09
+#define WB_QDSP_RAW_KERNEL_LENGTH               0x10
+
+struct firmware_v10_tag firmware_v10 {};
+struct firmware_v20_tag firmware_v20 {};
+
+struct  _QDSP_registers {
+
+	QDSP_registers(const firmware_v10_tag &);
+	QDSP_registers(const int, const int, const firmware_v20_tag &);
+
+	const uint32_t WB_QDSP_DEMOD_KERNEL_LENGTH;
+	
+	const uint32_t WB_QDSP_RAW_KERNEL_ADDR_DATA;
+	const uint32_t WB_QDSP_DEMOD_KERNEL_ADDR_DATA;
+
+	const uint32_t WB_QDSP_PHASE_INC;
+
+	const uint32_t WB_QDSP_THRESHOLD;
+	const uint32_t WB_QDSP_THRESHOLD_INVERT;
+	const uint32_t WB_QDSP_THRESHOLD_INPUT_SEL;
+
+	const uint32_t WB_QDSP_RAW_KERNEL_BIAS;
+	const uint32_t WB_QDSP_DEMOD_KERNEL_BIAS;
+
+	const uint32_t WB_QDSP_CORRELATOR_SIZE;
+	const uint32_t WB_QDSP_CORRELATOR_M_ADDR;
+	const uint32_t WB_QDSP_CORRELATOR_M_DATA;
+	const uint32_t WB_QDSP_CORRELATOR_SEL;
+
+} QDSP_registers;
+
+
+#endif
